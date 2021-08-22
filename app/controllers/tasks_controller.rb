792dc9a5d081cfbe0bef121cdeb6c15306a1b4f5
Aaron Battman <aaron.k.battman@outlook.com>
2021-08-22 17:45:13 +0100

    Single Model RAILS CRUD. Maunal routing. Refactor and partials implemented-(hard code left in psedo).

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,53 @@
 class TasksController < ApplicationController
-  # before action
+
+  before_action :dry_id_task, only: [:show, :edit, :update, :destroy]
+    # run privy meth first, which [:actions, :to_run]
+  
+  # ###ACTIONS###
   def index # all
     @tasks = Task.all
   end
-  
+
+  # def show; end
   def show
-    #raise
-    @task = Task.find(params[:id])
+  #  @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new # JUST INITIALIZES AN EMPTY INSTANCE FOR CREATE 
+  end
+  
+  def create
+    # raise
+    @task = Task.new(task_params) # CONVENTION APPEARS TO BE new & save - Don't .create!
+    @task.save
+    redirect_to task_path(@task)
   end
 
-  # def new
-  # end
+  # def edit; end
+  def edit
+    # @task = Task.find(params[:id])
+  end
 
-  # def create
-  # end
+  def update
+    # @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
 
-  # def edit
-  # end
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
 
-  # def update
-  # end
+  private
 
-  # def destroy
-  # end
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def dry_id_task
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,26 @@
 Rails.application.routes.draw do
   # DSL see https://guides.rubyonrails.org/routing.html
   # ###################################################
-  # all
+
+  # home
+  root to: 'tasks#index'
+
+  # resources :tasks, except: [] / only:[]
+  # show all
   get "tasks", to: "tasks#index"
+
   # create
+  get "tasks/new", to: "tasks#new", as: :new_task # NB. MUST COME BEFORE SHOW ONE (ID)
+  post "tasks", to: "tasks#create"
 
-  #one
+  # show one
   get "tasks/:id", to: "tasks#show", as: :task
-  #update
-
-  #destroy
-
+  
+  # update
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  
+  # destroy
+  delete "tasks/:id", to: "tasks#destroy"
 
 end
